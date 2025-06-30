2ab4cfea3442ec811d9a1920cc1e4c894231d2b7
zhangrenfeng <zhangrenfeng@horiscale.com>
2025-06-30 12:37:14 +0800

    delete unuseful code
    Signed-off-by: zhangrenfeng <zhangrenfeng@horiscale.com>

--- conftest.py
+++ conftest.py
@@ -20,30 +20,3 @@ def user_config(request):
 @pytest.fixture(scope="class", autouse=True)
 def set_user_config(request, user_config):
     request.cls.uconfig = user_config
-
-'''
-@pytest.fixture(scope="function")
-def login_context(request, user_config):
-    def setUp():
-        print('fixture setup')
-        tc_self = request.instance #tc_self: tes_class_self
-        uconfig = user_config
-        server_host = uconfig.get('server_host')
-        server_port = uconfig.get('server_port', 21)
-        server_user = uconfig.get('server_user')
-        server_password = uconfig.get('server_password')
-        timeout = uconfig.get('global_timeout', 5)
-        assert(server_host != None and server_user != None and server_password != None)
-        tc_self.client = tc_self.client_class(timeout=timeout)
-        tc_self.client.connect(server_host, server_port)
-        tc_self.client.login(server_user,server_password)
-    setUp()
-    yield
-    def tearDown():
-        print('fixture teardown')
-        tc_self = request.instance
-        tc_self.client.close()
-    tearDown()
-'''
-
-
